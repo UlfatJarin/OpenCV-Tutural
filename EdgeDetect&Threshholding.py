74748f2d8c18c2433378c7e061f553e7d6f06759
UlfatJarin <richiulfat@gmail.com>
2025-10-22 23:24:00 +0600

    6th

--- EdgeDetect&Threshholding.py
+++ EdgeDetect&Threshholding.py
@@ -44,4 +44,11 @@ find detecttion when light is changing  angwith many noice
 
 import cv2
 
-img = cv2. imread()
+img = cv2. imread("books.jpg", cv2.IMREAD_GRAYSCALE)
+
+edges = cv2.Canny(img, 50,150)
+
+cv2.imshow("orginal Image",img)
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

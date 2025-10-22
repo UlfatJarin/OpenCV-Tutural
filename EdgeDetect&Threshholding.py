89387f7102c91cddaeed6f6f30e6f0ab4a78958a
UlfatJarin <richiulfat@gmail.com>
2025-10-23 02:17:12 +0600

    7th

--- EdgeDetect&Threshholding.py
+++ EdgeDetect&Threshholding.py
@@ -43,12 +43,81 @@ find detecttion when light is changing  angwith many noice
 
 
 import cv2
+import numpy as np
 
-img = cv2. imread("books.jpg", cv2.IMREAD_GRAYSCALE)
+'''img = cv2. imread("bluebird.jpg", cv2.IMREAD_GRAYSCALE)
 
 edges = cv2.Canny(img, 50,150)
 
 cv2.imshow("orginal Image",img)
 cv2.imshow("Edges", edges)
 cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+
+
+
+# Thresholding in OpenCV
+"""
+ thresholded_image = cv2.threshold(image, thresh-value , max-value , method)
+                  img = gray scale
+                  thresh-value= cut off value (udually 0-255)[best - 100,120,150]
+                  max-value = assign if pic is bright enough (usually 255)
+                  mathod = most common -THRESH BINARY
+                            if bigger then Thresh value 
+                                then 255 - all white
+                            or       0   - all black
+
+ 
+"""
+'''img = cv2. imread("bluebird.jpg", cv2.IMREAD_GRAYSCALE)
+
+ret ,thresh_img = cv2.threshold(img,120 , 255, cv2.THRESH_BINARY)
+
+cv2.imshow("orginal Image",img)
+cv2.imshow("Edges",thresh_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+
+#Bitwise operation...........................................
+"""
+-combine 2 img 
+    - same img height & white
+    - grayscale / binary mask (white /black)
+    - 
+- cut out 1st image with 2nd img
+- flip specific img 
+- remove 
+# pixle 
+# #bitwise operation
+  1 cv2.bitwise_and(img1, img2)  - cutout shape 
+  2 cv2.bitwise_or(img1, img2)   -combime - multiple shape marge in one frame
+  3 cv2.bitwise_not(img1 )  - while to black vice varsa
+
+syntex - 
+
+
+"""
+img1 = np.zeros((300,300), dtype="uint8")
+img2 = np.zeros((300,300), dtype="uint8")
+
+cv2.circle(img1,(150,150),100, 255, -1)
+cv2.rectangle(img2,(100,100),(250,250),255 ,-1)
+
+bitwise_and =cv2.bitwise_and(img1,img2)
+bitwise_or =cv2.bitwise_or(img1,img2)
+bitwise_not =cv2.bitwise_not(img1)
+
+cv2.imshow("Circle",img1)
+cv2.imshow("Rectangle",img2)
+cv2.imshow("AND",bitwise_and)
+cv2.imshow("OR",bitwise_or)
+cv2.imshow("NOT",bitwise_not)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
 
 
-blurred = cv2.GaussianBlur(image ,(7,7), 3)
 
-cv2.imshow("Orginal image",image)
-cv2.imshow("Blurred image",blurred)
+
